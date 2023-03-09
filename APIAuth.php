ce5f7b37ae09439d0659c3f3e701133cbc0d8682
léo <leo.babaux@etu.iut-tlse3.fr>
2023-03-09 09:28:23 +0100

    API Authentification + utils

---
+++ APIAuth.php
@@ -0,0 +1,54 @@
+<?php
+/// Librairies nécéssaire
+include('jwt_utils.php');
+include('bd_utils.php')
+
+/// Paramétrage de l'entête HTTP (pour la réponse au Client)
+header("Content-Type:application/json");
+
+/// Identification du type de méthode HTTP envoyée par le client
+$http_method = $_SERVER['REQUEST_METHOD'];
+switch ($http_method){
+    /// Cas de la méthode POST, seul methode accepté
+    case "POST" :
+        /// Récupération des données envoyées par le Client
+        $postedData = file_get_contents('php://input');
+        $data = json_decode($postedData,true);
+
+        if (isset($data['login']) && isset($data['password'])){
+            if (checkUser($data['login'],$data['password'])){
+                $headers = array('alg'=>'HS256','typ'=>'JWT');
+                $payload = array('login'=>$data['login'],'role'=>getRole($data['login']),'exp'=>(time()+3600));
+
+                $jwt = generate_jwt($headers,$payload);
+
+                deliver_response(201, "Login success", $jwt);
+            } else {
+                deliver_response(401, "Login or password invalide", NULL);
+            }
+        } else {
+            deliver_response(400, "Missing login or password", NULL);
+        }
+
+        break;
+    /// Cas par defaut (methode autre que POST)
+    default :
+        deliver_response(405, "No corresponding method", NULL);
+    break;
+}
+
+/// Envoi de la réponse au Client
+function deliver_response($status, $status_message, $data){
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
+}
+?>
---
+++ bd_utils.php
@@ -0,0 +1,48 @@
+<?php
+    function bdLink(){
+        try {
+            $server = "127.0.0.1";
+            $db = "projetapi";
+            $login = "root";        
+                    
+            $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login);
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+        return $linkpdo;
+    }
+
+    function checkUser($login, $password){
+        $linkpdo = bdLink();
+
+        $req = $linkpdo -> prepare("SELECT password from Utilisateur where login=:login");
+        $res  = $req -> execute(array('login' => $login));
+
+        if($res == false){
+            $req -> debugDumpParams();
+                die('Erreur execute');
+        } else {
+            if($pwd = $req->fetch()) {
+                if ($pwd["password"]==sha1($password)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function getRole($login){
+        $linkpdo = bdLink();
+
+        $req = $linkpdo -> prepare("SELECT role from Utilisateur where login=:login");
+        $res  = $req -> execute(array('login' => $login));
+
+        if($res == false){
+            $req -> debugDumpParams();
+                die('Erreur execute');
+        } else {
+            $pwd = $req->fetch();
+            return $pwd["role"];
+        }
+    }
+?>
